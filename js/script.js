0d75af73590440113a63ff8292c329a36964cfa6
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2025-03-20 21:02:48 -0300

    Começando pagina principal

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Você pode adicionar interatividade com JS, caso queira que o menu se abra
+// ao clicar em vez de apenas ao passar o mouse.
+document.querySelectorAll('.dropdown > a').forEach(item => {
+    item.addEventListener('click', function(event) {
+      const submenu = this.nextElementSibling;
+      submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+      event.preventDefault();
+    });
+  });
+  
